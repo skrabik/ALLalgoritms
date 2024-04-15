69cf34172a2eda91aa79d7ab174510ab5e8decfb
skrabik <babenk00r@yandex.ru>
2024-04-15 22:41:45 +0300

    'contestasks'

--- ContestSFedU-2024/taskK.py
+++ ContestSFedU-2024/taskK.py
@@ -1,12 +1,36 @@
-n = int(input())
-data = [int(i) for i in input().split()]
-data = data[::-1]
-data[0] = 0
+def one_pol2(t1, t2, l1, l2):
+    lx1, ly1 = l1
+    lx2, ly2 = l2
 
-print(data)
-for i in range(1, n):
-    last = max(0, i - data[i])
+    x1, y1 = t1
+    x2, y2 = t2
 
-    data[i] = min(data[last], data[i-1]) + 1
+    a = ly1 - ly2
+    b = lx2 - lx1
+    c = (lx1*ly2) - (lx2*ly1)
 
-print(data[-1])
+    res1 = a*x1 + b*y1 + c
+    res2 = a*x2 + b*y2 + c
+
+
+    if res1*res2 == 0:
+        return True
+    elif res1*res2 < 0:
+        return True
+    else:
+        return False
+
+
+def intersection(t1, t2, t3, t4):
+    if one_pol2(t1, t2, t3, t4) and one_pol2(t3, t4, t1, t2):
+        return True
+    else:
+        return False
+
+t1 = (0, 0)
+t2 = (1, 2)
+t3 = (0, 3)
+t4 = (1, 1)
+
+# print(one_pol(t1, t2, t3, t4))
+print(intersection(t1, t2, t3, t4))
